b3d4900309c0ded544ac26cf1912612a55637af2
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-03-03 09:25:37 -0600

    added the modal now i have to add the other modals add them to the navigation bar whenever a button is clicked

--- Windows94/src/modals/windowsModal.jsx
+++ Windows94/src/modals/windowsModal.jsx
@@ -1,14 +1,35 @@
-import Draggable from 'react-draggable';
-
-
 export const WindowsModal = () => {
 
     return (
-        <Draggable>
-<div>
-            <h1>className="absolute left-32 top-32">miguel</h1>
+        <div className="h-96 w-96 bg-white border-2 border-l-gray border-t-gray absolute top-32 left-20 border-r-darkGray border-b-darkGray flex flex-col">
+            <div className="p-.05 flex justify-between bg-navyblue text-white font-display">
+                <span className="ml-1 h-1/2">Welcome to my page</span>
+                <div className="py-1 h-5 flex items-center">
+                    <button className="bg-grayish p-1 mx-1 h-4 text-black border-2 border-t-gray border-l-gray border-r-darkGray border-b-darkGray flex items-center justify-center text-xl font-bold">_</button>
+                    <button className="bg-grayish p-1 mx-1 h-4 text-black border-2 border-t-gray border-l-gray border-r-darkGray border-b-darkGray flex items-center justify-center text-lg font-bold ">X</button>
+                </div>
+            </div>
+            <div className="bg-grayish flex flex-col flex-1">
+                <ul className="flex text-black font-display">
+                    <li className="mx-1"><span className="underline">F</span>ile</li>
+                    <li className="mx-1"><span className="underline">E</span>dit</li>
+                    <li className="mx-1"><span className="underline">V</span>iew</li>
+                    <li className="mx-1"><span className="underline">O</span>ptions</li>
+                    <li className="mx-1"><span className="underline">H</span>elp</li>
+                </ul>
+                <div className="mx-1 border-2 border-t-darkGray border-b-gray border-r-gray border-l-darkGray">
+                </div>
+                <div className="h-full flex-1 my-1">
+                    <div className="h-full bg-white text-center mx-1 py-2 border-2 border-t-black border-l-black border-r-gray border-b-gray">
+                        <h1 className="text-black font-display p-1">Hi im Miguel </h1>
+                        <p className="text-black font-display p-1">DEVELOPER AND DESIGNER</p>
+                        <p className="text-black font-display p-1">I'm fascinated by creative technology, and love to create visually immersive user experiences with code</p>
+                        <p className="text-black font-display p-1">I especially love web + mobile development, UI/UX and everything that comes with it!</p>
+                    </div>
+                </div>
+                <div className="font-display mx-1 border-2 border-t-darkGray border-l-darkGray border-r-gray border-b-gray mb-1 text-xl">3 Objects</div>
+            </div>
         </div>
-        </Draggable>
-        
     )
 }
+
