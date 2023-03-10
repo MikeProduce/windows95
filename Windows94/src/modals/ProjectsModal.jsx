0a632690e31b36e0afd1931175736a8b1212d2f2
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-03-10 09:36:02 -0600

    Hyped was able to successfully transfer code from my other project

--- Windows94/src/modals/ProjectsModal.jsx
+++ Windows94/src/modals/ProjectsModal.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import { useDispatch, useSelector} from 'react-redux';
 import { addToCart, removeToCart, toggleHidden } from '../Redux/cartSlice.jsx';
 import { ModalLayout } from './ModalLayout.jsx';
+import  projectData from '../JSON/projects.json'
 
 
 
@@ -9,6 +10,8 @@ export const ProjectsModal = ({onClose}) => {
     const dispatch = useDispatch();
     const [modalVisibility, setmodalVisibility] = useState('hidden'); // Add state variable
     const {cart} = useSelector((state) => state.cart)
+    const [projects, setProjects] = useState(projectData);
+    const projectsArr = projects.projects;
 
     const addToCartHandler = ( ) => {
         const itemObj = { itemName: 'Projects' };
@@ -38,11 +41,21 @@ export const ProjectsModal = ({onClose}) => {
       return (
         <div className={`h-62 w-62 top-96 sm:h-96 absolute bg-white border-2 border-l-gray border-t-gray border-r-darkGray border-b-darkGray flex flex-col ${modalVisibility}`}>
         <ModalLayout TitleDescription="Projects" addToCartHandler={addToCartHandler} onCloseModal={onCloseModal}>
-        <h1 className="text-black font-display p-1">Hi im Miguel </h1>
-                 <p className="text-black font-display p-1 break-words">DEVELOPER AND DESIGNER</p>
-                 <p className="text-black font-display p-1 break-words">I'm fascinated by creative technology, and love to create visually immersive user experiences with code</p>
-                 <p className="text-black font-display p-1 break-words">I especially love web + mobile development, UI/UX and everything that comes with it!</p>
-                
+        <section className='py-20 md:px-20  px-5 lg:px-20 bg-white lg:px-20'>
+      <h1 className='text-5xl text-center font-semibold'>Projects</h1>
+      <p className='text-center mb-4'>Things I've Built</p>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 flex justify-center'>
+        {projectsArr.map((project, index) => (
+            <div className='m-5 bg-Very-Light-Gray mt-10 rounded-xl transition hover:scale-110 cursor-pointer hover:drop-shadow-xl' key={index}>
+              <img className='rounded-t-xl w-full object-cover' src={project.image} alt={project.image} />
+              <h1 className='text-xl text-center p-4'>{project.title}</h1>
+              <h1 className='text-md px-6 p-4'>{project.description}</h1>
+              <p className='p-4'>Tech Stack: {project.techStack}</p>
+              <a href={project.github} className='p-4 text-sm text-Grayish-Blue'>GitHub Repo</a>
+              <a href={project.liveSite} className='p-4  text-sm text-Grayish-Blue'>LiveSite</a>
+            </div>))}
+      </div>
+    </section> 
      </ModalLayout>
      </div>
     )
