16d03175c8d57f198ec3b8d6f254a0d714662e73
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-03-18 15:07:37 -0500

    added the rest of the projects

--- Windows94/src/modals/ProjectsModal.jsx
+++ Windows94/src/modals/ProjectsModal.jsx
@@ -13,7 +13,7 @@ export const ProjectsModal = () => {
 
 
       return (
-        <div className={`mx-2 h-3/4 w-62 top-24 sm:h-1/2 sm:w-3/4 absolute bg-white border-2 border-l-gray border-t-gray border-r-darkGray border-b-darkGray flex flex-col ${modalVisibility}`}>
+        <div className={`mx-2 h-3/4 w-62 top-24 sm:h-3/4 sm:w-11/12 absolute bg-white border-2 border-l-gray border-t-gray border-r-darkGray border-b-darkGray flex flex-col ${modalVisibility}`}>
             <ModalLayout 
                 TitleDescription={windowName} 
                 addWindowHandler={addWindowHandler} 
@@ -21,16 +21,16 @@ export const ProjectsModal = () => {
                 >
                 <div className='h-full flex flex-col  overflow-y-auto bg-gray'>
                     <h1 className='text-5xl text-center font-display'>Projects</h1>
-                    <p className='text-center mb-4 font-display'>Things I've Built</p>
+                    <p className='text-center mb-4 font-display text-2xl'>Things I've Built</p>
                     <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 flex justify-center font-display mx-5'>
                         {projectsArr.map((project, index) => (
-                            <div className='m-5 bg-Very-Light-Gray mt-10 rounded-xl transition hover:scale-110 cursor-pointer hover:drop-shadow-xl' key={index}>
+                            <div className='m-5 bg-Very-Light-Gray mt-10 rounded-xl mx-2 border-2 border-black pb-4' key={index}>
                                 <img className='rounded-t-xl w-full object-cover' src={project.image} alt={project.image} />
                                 <h1 className='text-xl text-center p-4'>{project.title}</h1>
                                 <h1 className='text-md px-6 p-4'>{project.description}</h1>
                                 <p className='p-4'>Tech Stack: {project.techStack}</p>
-                                <a href={project.github} className='p-4 text-sm text-Grayish-Blue'>GitHub Repo</a>
-                                <a href={project.liveSite} className='p-4  text-sm text-Grayish-Blue'>LiveSite</a>
+                                <a href={project.github} className='mx-2 border-2 border-black py-2 px-4 rounded-md text-sm text-Grayish-Blue hover:scale-110 transition cursor-pointer hover:drop-shadow-xl hover:bg-black hover:text-white'>GitHub Repo</a>
+                                <a href={project.liveSite} className='border-2 border-black py-2 px-4 rounded-md text-sm text-Grayish-Blue hover:scale-110 transition cursor-pointer hover:drop-shadow-xl hover:bg-black hover:text-white'>LiveSite</a>
                             </div>
                         ))}
                     </div>
